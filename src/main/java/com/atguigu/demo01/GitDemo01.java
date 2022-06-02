5f26984612fb654c56068650a3faab0eaf5f9e07
小胡 <1415386591@qq.com>
2022-06-02 11:31:30 +0800

    第三次更新

--- src/main/java/com/atguigu/demo01/GitDemo01.java
+++ src/main/java/com/atguigu/demo01/GitDemo01.java
@@ -4,6 +4,5 @@ public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("你好1");
         System.out.println("你好2");
-        System.out.println("你好3");
     }
 }
